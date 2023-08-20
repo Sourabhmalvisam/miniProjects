2bbdd72b0228366f62ee21ef16ecf8097ac12612
Sourabh Malvi Sam <samsourabh786@gmail.com>
2023-08-20 16:22:04 +0530

    added comments

--- Python/GithubProfilePhoto.py
+++ Python/GithubProfilePhoto.py
@@ -1,8 +1,22 @@
-import requests
-from bs4 import BeautifulSoup as bs
-user_name=input("Enter your Github Profile Username: ")
-url="https://github.com/"+user_name
-r=requests.get(url)
-soup=bs(r.content,"html.parser")
-profile_image=soup.find("img",{"alt":"Avatar"})["src"]
+# Import the necessary libraries
+import requests  # For making HTTP requests
+from bs4 import BeautifulSoup as bs  # For parsing HTML content
+
+# Prompt the user to input their GitHub profile username
+user_name = input("Enter your GitHub Profile Username: ")
+
+# Construct the URL of the user's GitHub profile
+url = "https://github.com/" + user_name
+
+# Send an HTTP GET request to the GitHub profile URL
+r = requests.get(url)
+
+# Parse the HTML content of the response using BeautifulSoup
+soup = bs(r.content, "html.parser")
+
+# Find the HTML element containing the user's profile image (avatar)
+# The "alt" attribute of the <img> tag is set to "Avatar"
+profile_image = soup.find("img", {"alt": "Avatar"})["src"]
+
+# Print the URL of the user's profile image (avatar)
 print(profile_image)
